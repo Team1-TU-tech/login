394dddca0bd29defed975085f38030d9d111b51d
sunwoo <summerham22@gmail.com>
2024-10-24 15:01:20 +0900

    docker compose and bug fix

--- login/src/main/java/com/login/login/controller/ApiController.java
+++ login/src/main/java/com/login/login/controller/ApiController.java
@@ -1,13 +1,7 @@
 package com.login.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.login.login.entity.LoginEntity;
 import com.login.login.service.LoginService;
@@ -15,47 +9,48 @@ import com.login.login.service.LoginService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/login")
 public class ApiController {
 
     @Autowired
     LoginService loginService;
 
-    @GetMapping("/login")
+    // 모든 로그인 정보 조회
+    @GetMapping
     public List<LoginEntity> list() {
-        System.out.println("[Controller]");
-        List<LoginEntity> r = loginService.getLogin();
-        return r;
+        System.out.println("[Controller] get all logins");
+        return loginService.getLogin();
     }
 
-    @GetMapping("/login")
+    // 특정 ID의 로그인 정보 조회 (RequestParam 사용)
+    @GetMapping("/find")
     public LoginEntity find(@RequestParam String id) {
-        LoginEntity r = loginService.findById(id);
-        return r;
+        System.out.println("[Controller] find login by id: " + id);
+        return loginService.findById(id);
     }
 
     // C - INSERT
-    @PostMapping("/login")
+    @PostMapping
     public void createLogin(@RequestBody LoginEntity loginEntity) {
-        System.out.println("[Controller]" + loginEntity.toString());
+        System.out.println("[Controller] Create login: " + loginEntity);
         loginService.createLogin(loginEntity);
-        System.out.println("INSERT SUCCESSED");
-
+        System.out.println("INSERT SUCCESSFUL");
     }
 
     // U - UPDATE
-    @PutMapping("/login")
-    public void updateLogin(@RequestParam String id, @RequestBody LoginEntity loginEntity) {
+    @PutMapping("/{id}")
+    public void updateLogin(@PathVariable String id, @RequestBody LoginEntity loginEntity) {
+        System.out.println("[Controller] Update login with id: " + id);
         loginEntity.setId(id);
         loginService.updateLoginById(id, loginEntity);
-        System.out.println("UPDATE SUCCESSED");
-
+        System.out.println("UPDATE SUCCESSFUL");
     }
 
     // D - DELETE
-    @DeleteMapping("/login")
-    public void deleteLogin(@RequestParam String id) {
+    @DeleteMapping("/{id}")
+    public void deleteLogin(@PathVariable String id) {
+        System.out.println("[Controller] Delete login with id: " + id);
         loginService.deleteLoginById(id);
-        System.out.println(id + "DELETE SUCCESSED");
-
+        System.out.println("DELETE SUCCESSFUL");
     }
 }
